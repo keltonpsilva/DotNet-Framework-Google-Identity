1f1fd35ba751a16fcdf69ecb9404580f982feb22
Kelton <kelton.jesus23@gmail.com>
2021-08-07 00:16:12 +0100

    Add DependencyInjection using Ninject

---
+++ DotNet-Framework-Google-Identity/Infrastructure/DependencyInjection.cs
@@ -0,0 +1,12 @@
+﻿using Ninject.Modules;
+
+namespace DotNet_Framework_Google_Identity.Infrastructure
+{
+    public class DependencyInjection : NinjectModule
+    {
+        public override void Load()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
